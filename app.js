a62c4e96ddb1c29fb3a93d365bb17aa65b48f4ab
VictorTTT <cotirena@gmail.com>
2020-11-23 13:00:22 +0100

    último commit

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-window.onload = iniciar;
-
-function iniciar() {
-}
-
-
 //Crear 3 arrays con el nombre de las imágenes adjuntas en cada una de las posiciones.
 var array1 = ['aguacate', 'ajo', 'cebolla', 'pepino', 'puerro', 'tomate', 'zanahoria'];
 var array2 = ['aguacate', 'ajo', 'cebolla', 'pepino', 'puerro', 'tomate', 'zanahoria'];
 var array3 = ['aguacate', 'ajo', 'cebolla', 'pepino', 'puerro', 'tomate', 'zanahoria'];
 
-
-/*El usuario tiene que introducir antes de empezar la partida un número de monedas con el que quiere empezar a jugar,
- no podrá volver a introducir monedas, por lo que tendremos que desactivar la opción de insertar más.*/
+//Variables necesarias
 var monedas = 0;
 var tirada = 1;
 var euro = "<img src='img/euro.png' alt='moneda de euro'>";
 var premio = '';
 
-
+// Función que actúa cuando presionamos el botón introducir monedas
 function incrementaMonedas() {
     monedas++;
     document.getElementById('imprimeMonedas').innerHTML = monedas + ' ' + euro;
 }
 
 
-//Añadir un botón "Tirar", en este momento gastará una moneda y mostrará las tres imágenes aleatorias.
+// Funcíon que actúa cuando presionamos el botón tirar
 function empezarJuego() {
     if (monedas === 0) {
         alert('Introduzca monedas para jugar');
@@ -36,21 +28,17 @@ function empezarJuego() {
         monedas--;
 
         //Declaramos los slots y les asignamos una imagen de cada array escogida de forma aleatoria
-         var slot1 = (array1[Math.floor(Math.random() * 7)]);
-         var slot2 = (array2[Math.floor(Math.random() * 7)]);
-         var slot3 = (array3[Math.floor(Math.random() * 7)]);
-
-
+        var slot1 = (array1[Math.floor(Math.random() * 7)]);
+        var slot2 = (array2[Math.floor(Math.random() * 7)]);
+        var slot3 = (array3[Math.floor(Math.random() * 7)]);
 
         //Imprimimos cada imagen en el lugar correspondiente del HTML
         document.getElementById("imagen1").src = "img/" + slot1 + ".png";
         document.getElementById("imagen2").src = "img/" + slot2 + ".png";
         document.getElementById("imagen3").src = "img/" + slot3 + ".png";
 
-
-
-
-        document.getElementById('mensajePremio').innerHTML = 'PREMIO: ' + premio;
+        //Vaciamos de la pantalla el premio anterior
+        document.getElementById('mensajePremio').innerHTML = 'PREMIO: ';
 
         //Si salen tres zanahorias, se ganan diez monedas
         if (slot1 === 'zanahoria' && slot2 === 'zanahoria' && slot3 === 'zanahoria') {
@@ -66,7 +54,7 @@ function empezarJuego() {
             premio = euro + euro + euro + euro;
             document.getElementById('mensajePremio').innerHTML = 'PREMIO: ' + euro + ' x 4';
         }
-        //si sale una zanahoria y 2 hortalizas iguales
+        //si sale una zanahoria y 2 hortalizas iguales, se ganan 3 monedas
         else if (slot1 === slot2 && slot3 === 'zanahoria' ||
             slot2 === slot3 && slot1 === 'zanahoria' ||
             slot1 === slot3 && slot2 === 'zanahoria') {
@@ -80,7 +68,6 @@ function empezarJuego() {
             premio = euro;
             document.getElementById('mensajePremio').innerHTML = 'PREMIO: ' + euro + ' x 1';
         }
-
         //Si salen tres hortalizas iguales que no sean zanahorias, se ganan cinco monedas
         else if (slot1 === slot2 && slot2 === slot3) {
             monedas += 5;
@@ -94,14 +81,14 @@ function empezarJuego() {
             document.getElementById('mensajePremio').innerHTML = 'PREMIO: ' + euro + ' x 2';
         }
 
-
+        // Actualizamos el contador de monedas después de la tirada
         document.getElementById('imprimeMonedas').innerHTML = monedas + ' ' + euro;
 
 
-        //Acceso a la tabla HTMLmedianteel Id
+        //Acceso a la tabla de tiradas mediante el Id
         var tabla = document.getElementById('filaTirada');
 
-        //Creamos elemento HTML desde JS
+        //Creamos elemento  fila HTML desde JS
         var fila = document.createElement('tr');
 
         //En el interior de la fila incluimos las celdas
@@ -113,45 +100,11 @@ function empezarJuego() {
 
         // Finalmente, para cada tirada, añadimos una fila en la tabla
         tabla.appendChild(fila);
-        premio = '';
-
     }
 }
 
-// Mostrar en formato lista el historial de monedas ganadas y perdidas en cada tirada.
-
-
-//Añadir un botón "Salir", para que muestre en una alerta el número total de monedas.
+//Función que actúa cuando presionamos el botón salir
 function salirJuego() {
     alert('En total hay  ' + monedas + ' monedas');
+    location.reload();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
